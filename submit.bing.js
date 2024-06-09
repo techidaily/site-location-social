3bbfd31b26d76d6271d1cbcc618ce6d439331d90
lauer3912 <lauer3912@gmail.com>
2024-06-09 11:03:48 +0800

    auto backup on 2024-06-09T03:03:47.301Z

--- submit.bing.js
+++ submit.bing.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 
-const indexNowKey = 'ec9c9516ea82433086d979e16e1960e0'; // 替换为你的API密钥
+const indexNowKey = 'e1bdd5060f3b451cb4cd2f8a398bedda'; // 替换为你的API密钥
 const siteHostName = 'android-transfer.techidaily.com'; // 替换为你的网站URL
 const groupItemCount = 100; // 每组最多个URL
 
